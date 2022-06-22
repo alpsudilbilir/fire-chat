9c935b817dcf45f47b0dd09ece55c664cee14b95
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-06-23 01:42:34 +0300

    Initial commit.

---
+++ FireChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireChat
+//
+//  Created by Alpsu Dilbilir on 23.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Alpsu Dilbilir on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
