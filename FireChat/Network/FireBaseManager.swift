ddec8fbf70415448a9d24865309697826f195a34
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-06-26 17:30:28 +0300

    Implementation of user data storage.

--- FireChat/Network/FireBaseManager.swift
+++ FireChat/Network/FireBaseManager.swift
@@ -8,32 +8,34 @@
 import Foundation
 import Firebase
 import FirebaseStorage
+import FirebaseFirestore
 import UIKit
 
 class FireBaseManager {
-
+    
     func createNewAccount(email: String, password: String, image: UIImage) {
         Auth.auth().createUser(withEmail: email, password: password) { result, err in
             if let err = err {
                 print("Failed to create user:", err)
                 return
             }
-            self.saveImageToStorage(image: image)
+            self.saveImageToStorage(email: email, image: image)
             print("Successfully created user: \(result?.user.uid ?? "")")
+            
         }
     }
     
     func loginUser(email: String, password: String) {
         Auth.auth().signIn(withEmail: email, password: password) { res, err in
             if let err = err {
-                print("Failed to login.")
+                print("Failed to login. \(err)")
                 return
             }
             print("Succesfully logged in.")
         }
     }
     
-    func saveImageToStorage(image: UIImage) {
+    func saveImageToStorage(email: String, image: UIImage) {
         //let filename = UUID().uuidString
         guard let uid = Auth.auth().currentUser?.uid else { return }
         let ref = Storage.storage().reference(withPath: uid)
@@ -49,8 +51,22 @@ class FireBaseManager {
                     return
                 }
                 print("Successfully stored image with url.")
+                guard let url = url else { return }
+                self.saveUserInfo(email: email, imageUrl: url)
             }
         }
-        
+    }
+    
+    func saveUserInfo(email: String, imageUrl: URL) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let userData = ["email": email, "uid": uid, "imageUrl": imageUrl.absoluteString]
+
+        Firestore.firestore().collection("users").document(uid).setData(userData) { err in
+            if let err = err {
+                print("Failed to save user info. \(err)")
+                return
+            }
+            print("Succesfully user data is stored!")
+        }
     }
 }
