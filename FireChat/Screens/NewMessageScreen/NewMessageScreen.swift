eadca4fd4b621e381e975fecf05db4364fb2bbc6
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-07-01 03:51:21 +0300

    Add new  message screen and view model.

--- FireChat/Screens/NewMessageScreen/NewMessageScreen.swift
+++ FireChat/Screens/NewMessageScreen/NewMessageScreen.swift
@@ -6,10 +6,50 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct NewMessageScreen: View {
+    @Environment(\.presentationMode) var dismiss
+    @ObservedObject var viewModel = NewMessageScreenViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                ForEach(viewModel.users, id:\.uid) { user  in
+                    HStack {
+                        WebImage(url: URL(string: user.imageUrl ?? "https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png"))
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 52, height: 52)
+                            .cornerRadius(52)
+                            .clipped()
+                            .overlay(Circle().stroke(lineWidth: 2).foregroundColor(.fire))
+                        NavigationLink {
+                            Text("Destination")
+                        } label: {
+                            Text(user.email)
+                                .font(.title3)
+                                .foregroundColor(Color(.label))
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    .padding(.vertical, 5)
+                    Divider()
+                }
+                .navigationTitle("Users")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss.wrappedValue.dismiss()
+                        } label: {
+                            Text("Cancel")
+                                .foregroundColor(.fire)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- FireChat/Screens/NewMessageScreen/NewMessageScreenViewModel.swift
+++ FireChat/Screens/NewMessageScreen/NewMessageScreenViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+
+class NewMessageScreenViewModel: ObservableObject {
+    @Published var users = [User]()
+    init() {
+        fetchAllUsers()
+    }
+        private func fetchAllUsers() {
+            FireBaseManager.shared.firestore.collection("users").getDocuments { snapshot, err in
+                if let err = err {
+                    print("Failed to fetch users. \(err)")
+                    return
+                }
+                snapshot?.documents.forEach({ snapshot in
+                    let data = snapshot.data()
+                    let uid = data["uid"] as? String ?? ""
+                    let email = data["email"] as? String ?? ""
+                    let imageUrl = data["imageUrl"] as? String ?? ""
+                    self.users.append(User(uid: uid, email: email, password: "", imageUrl: imageUrl))
+                    print("Users successfully fetched.")
+
+                })
+            }
+        }
+    }
