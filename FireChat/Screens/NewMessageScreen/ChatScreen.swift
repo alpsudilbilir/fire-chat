e48c5b4bf83c0abcf826704da6914714499eaadf
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-07-05 00:48:48 +0300

    Design user interface of ChatScreen.

--- FireChat/Screens/NewMessageScreen/ChatScreen.swift
+++ FireChat/Screens/NewMessageScreen/ChatScreen.swift
@@ -6,23 +6,98 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ChatScreen: View {
     let user: User
+    @State var message: String = ""
+    @State var messages = [String]()
+    @State var placeholder = "Message"
+    @State var isSendButtonDisabled = false
     
     var body: some View {
-        ScrollView {
-            ForEach(0..<100) { message in
-                Text("Fake messages here")
-            }
+        
+        VStack {
+            messageView
+            bottomBar
+                .padding(.vertical)
         }
         .navigationTitle("\(user.email)")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                WebImage(url: URL(string: user.imageUrl ?? "https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png"))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 34, height: 34)
+                    .cornerRadius(34)
+                    .clipped()
+                    .overlay(Circle().stroke(lineWidth: 2).foregroundColor(.fire))
+            }
+        }
+    }
+    private var messageView: some View {
+        ScrollView {
+            ScrollViewReader { value in
+            ForEach(messages, id: \.self) { message in
+                HStack {
+                    Spacer()
+                    HStack {
+                        Text(message)
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    .background(Color.fire)
+                    .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                .padding(.top, 8)
+            }
+            .onAppear {
+                value.scrollTo(messages.last, anchor: .center)
+            }
+            }
+        }
+    }
+    private var bottomBar: some View {
+        HStack(spacing: 10) {
+            Image(systemName: "photo.on.rectangle.angled")
+                .font(.system(size: 32))
+                .foregroundColor(.gray)
+            TextEditor(text: $message)
+                .frame(width: .infinity, height: 32, alignment: .center)
+                .overlay {
+                    HStack {
+                        Text(placeholder)
+                            .foregroundColor(.gray)
+                        Spacer()
+                    }
+                }
+                .onTapGesture {
+                    placeholder = ""
+                   
+                    
+                }
+            Button {
+                if !message.isEmpty {
+                    messages.append(message)
+                    message = ""
+                }
+                
+            } label: {
+                Image(systemName: "paperplane.circle.fill")
+                    .font(.system(size: 32))
+                    .foregroundColor(isSendButtonDisabled ? .gray : .fire)
+            }.disabled(isSendButtonDisabled)
+            Spacer()
+        }.padding(.horizontal)
     }
 }
-
 struct ChatScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ChatScreen(user: User())
+        NavigationView {
+            ChatScreen(user: User(uid: "aDS", email: "alpsudilbilir@gmail.com", password: "123", imageUrl: nil))
+        }
+        
     }
 }
